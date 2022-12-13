23955cc634cdaed09a3736070af236cbf00b0f38
Jakub Kosinka <kosinka@spstrutnov.cz>
2022-12-13 11:48:21 +0100

    Uprava

--- src/www/js/script.js
+++ src/www/js/script.js
@@ -5,87 +5,53 @@ let operandi
 let vypocitan = false;
 
 function cislo(vstup) {
-    if (vypocitan == true){
-        cislo1 = 0;
+    if (vypocitan == true) {
+        cislo2 = cislo1;
+        cislo1 = vstup;
+        display.innerHTML = cislo1;
         vypocitan = false;
     }
-    if(cislo1 == 0){
-        cislo1 = vstup;
-    }else{
-    cislo1 = cislo1 + vstup.toString();}
-    display.innerHTML = cislo1;
-}
-
-function scitani(){
-    if (cislo2 == 0 && vypocitan==false){
-        cislo2 = cislo1;
-        cislo1 = 0;
-        operandi = "+";}
-    else{
-        vysledek();
-        operandi = "+";
-        cislo1 = 0;
-    }
-}
-
-function odcitani(){
-    if (cislo2 == 0){
-        cislo2 = cislo1;
-        cislo1 = 0;
-        operandi = "-";}
-    else{
-        vysledek();
-        operandi = "-";
-        cislo1 = 0;
-    }
-}
-
-function nasobeni(){
-    if (cislo2 == 0){
-        cislo2 = cislo1;
-        cislo1 = 0;
-        operandi = "*";}
-    else{
-        vysledek();
-        operandi = "*";
-        cislo1 = 0;
-    }
-}
-
-function deleni(){
-    if (cislo2 == 0){
-        cislo2 = cislo1;
-        cislo1 = 0;
-        operandi = "/";}
-    else{
-        vysledek();
-        operandi = "/";
-        cislo1 = 0;
+    else {
+        cislo1 = cislo1.toString + vstup.toString();
+        display.innerHTML = cislo1;
     }
 }
 
 function vysledek(){
-    if(operandi == "+"){
-        cislo2 = parseFloat(cislo1) + parseFloat(cislo2);
-        display.innerHTML = cislo2;
-        vypocitan = true;
+    if (operandi == "+"){
+        cislo1 = Number(cislo2) + Number(cislo1);
+    }else if (operandi == "-"){
+        cislo1 = Number(cislo2) - Number(cislo1);
+    }else if (operandi == "*"){
+        cislo1 = Number(cislo2) * Number(cislo1);
+    }else if (operandi == "/"){
+        cislo1 = Number(cislo2) / Number(cislo1);
     }
-    else if(operandi == "-"){
-        cislo2 = parseFloat(cislo2) - parseFloat(cislo1);
-        display.innerHTML = cislo2;
-        vypocitan = true;
+    else{
+        cislo2 = cislo1;
     }
-    else if(operandi == "*"){
-        cislo2 = parseFloat(cislo2) * parseFloat(cislo1);
-        display.innerHTML = cislo2;
-        vypocitan = true;
-    }
-    else if(operandi == "/"){
-        cislo2 = parseFloat(cislo2) / parseFloat(cislo1);
-        display.innerHTML = cislo2;
-        vypocitan = true;
-    }
+    display.innerHTML = cislo1;
+    vypocitan = true;
+}
 
+function scitani(){
+    vysledek();
+    operandi = "+";
+}
+
+function odcitani(){
+    vysledek();
+    operandi = "-";
+}
+
+function nasobeni(){
+    vysledek();
+    operandi = "*";
+}
+
+function deleni(){   
+    vysledek();
+    operandi = "/";
 }
 
 function plusminus(){
